67cdf024c8a7d6c4e891fdf868cce61a2586fa84
Kevin Downey <redchin@gmail.com>
2013-02-06 01:08:48 -0800

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ed25519 "2.0.0-SNAPSHOT"
+(defproject ed25519 "1.1.0"
   :description "port of python ed25519 library"
   :url "https://github.com/hiredman/ed25519"
   :dependencies [[org.clojure/clojure "1.4.0"]]

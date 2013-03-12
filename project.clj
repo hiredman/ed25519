11462f2576ed5aa2fd26797394bd8c88fa8d4bc7
Kevin Downey <redchin@gmail.com>
2013-03-12 06:52:49 -0700

    clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject ed25519 "2.0.0-SNAPSHOT"
   :description "port of python ed25519 library"
   :url "https://github.com/hiredman/ed25519"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :test-selectors {:default (complement :regression)
                    :regression :regression
                    :all (constantly true)}

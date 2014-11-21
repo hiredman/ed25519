e222d38e4492ef8c7552a49d39b6c75fb17b64e9
Kevin Downey <redchin@gmail.com>
2014-11-21 08:02:46 -0800

    Revert "get rid of new reflection warnings"
    This reverts commit 6901cd3932933af389b2c98e4487c075da3dfbc5.

--- src/ed25519/replacements.clj
+++ src/ed25519/replacements.clj
@@ -11,21 +11,19 @@
     (let [x (rest value)
           [low high] (if (= 2 (count x))
                        x
-                       [0 (first x)])
-          a (with-meta (gensym 'a)
-              {:tag 'objects})]
+                       [0 (first x)])]
       `(let [h# ~high
              l# ~low
              c# (- h# l#)
-             ~a (make-array Object c#)]
+             a# (make-array Object c#)]
          (loop [array-index# 0
                 n# l#]
            (if (> c# array-index#)
              (let [~name n#]
-               (aset ~a array-index# ~body)
+               (aset a# array-index# ~body)
                (recur (inc array-index#)
                       (inc n#)))
-             ~a))))
+             a#))))
     `(clojure.core/for ~binding ~body)))
 
 (defmacro + [a b]
@@ -76,7 +74,7 @@
   `(throw (Exception. ~s)))
 
 (defmacro gen-concat [& args]
-  (let [arr (with-meta (gensym 'arr) {:tag 'objects})]
+  (let [arr (gensym 'arr)]
     `(let [size# ~(conj (for [x args] `(count ~x)) 'clojure.core/+)
            ~arr (make-array Object size#)
            ~'a-i 0
@@ -85,7 +83,7 @@
                                    arr-i# ~'a-i]
                               (if (> (count ~a) a-i#)
                                 (do
-                                  (aset ~arr arr-i# (aget ~(with-meta a {:tag 'objects}) a-i#))
+                                  (aset ~arr arr-i# (aget ~a a-i#))
                                   (recur (inc a-i#) (inc arr-i#)))
                                 arr-i#))]]
                x)]
